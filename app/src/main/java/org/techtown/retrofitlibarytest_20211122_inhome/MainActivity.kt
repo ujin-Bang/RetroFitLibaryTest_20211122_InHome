2c31c82cf3fd6fe34ba0c0f7a1e7eea6e50cff82
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2021-11-22 22:15:46 +0900

    액티비티 기본 구조 설정

--- app/src/main/java/org/techtown/retrofitlibarytest_20211122_inhome/MainActivity.kt
+++ app/src/main/java/org/techtown/retrofitlibarytest_20211122_inhome/MainActivity.kt
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 
- class MainActivity : AppCompatActivity() {
+ class MainActivity : BaseActivity() {
+
 
     lateinit var binding :AppCompatActivity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+       binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setValues()
+        setupEvents()
     }
-}
+
+     override fun setupEvents() {
+
+     }
+
+     override fun setValues() {
+
+     }
+
+
+ }
