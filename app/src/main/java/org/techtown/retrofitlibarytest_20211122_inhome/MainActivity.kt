107337c71347c2b35eebecca02a611c22fdbf6b7
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2021-11-22 22:05:52 +0900

    BaseActivity 기본 작업

--- app/src/main/java/org/techtown/retrofitlibarytest_20211122_inhome/MainActivity.kt
+++ app/src/main/java/org/techtown/retrofitlibarytest_20211122_inhome/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
 
     lateinit var binding :AppCompatActivity
     override fun onCreate(savedInstanceState: Bundle?) {
